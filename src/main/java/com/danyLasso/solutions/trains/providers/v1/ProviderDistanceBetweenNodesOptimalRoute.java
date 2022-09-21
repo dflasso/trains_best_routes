5d477106360730268720ffaee478fa9a0d67c419
dlassoa <dlasso@ec.krugercorp.com>
2022-09-21 10:46:05 -0500

    fixed infinity loop when find optimal route

--- src/main/java/com/danyLasso/solutions/trains/providers/v1/ProviderDistanceBetweenNodesOptimalRoute.java
+++ src/main/java/com/danyLasso/solutions/trains/providers/v1/ProviderDistanceBetweenNodesOptimalRoute.java
@@ -13,6 +13,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import com.danyLasso.solutions.trains.providers.ProviderDistanceBetweenNodes;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -39,7 +41,7 @@ public class ProviderDistanceBetweenNodesOptimalRoute extends ProviderRouteBase
 
         //analyze routes
         Optional<RouteDTO> routeOpt = this.findOptimalRoute(origin, destiny, edges);
-        
+
         return routeOpt.isPresent() ? routeOpt.get().getDistanceAccumulatedFinal() : 0D;
     }
 
@@ -52,54 +54,87 @@ public class ProviderDistanceBetweenNodesOptimalRoute extends ProviderRouteBase
      */
     private Optional<RouteDTO> findOptimalRoute(NodeDTO origin, NodeDTO destiny, List<EdgeDTO> edges) {
         Optional<RouteDTO> optimalRoute = Optional.empty();
-        
+
         RouteDTO initRoute = this.buildInitRoute(origin);
         RouteDTO selectedRoute = initRoute;
-        
+
         List<RouteDTO> routes = new ArrayList<>();
         routes.add(initRoute);
-        
-        List<RouteDTO> newRoutes = new ArrayList<>();    
-        
+
+        List<RouteDTO> newRoutes = new ArrayList<>();
+
         List<RouteDTO> routesFound = new ArrayList<>();
-        
+
         Integer numRoutesToAnalyze = routes.size();
         Double minDistance;
-        
-        while (numRoutesToAnalyze > 0) {          
+
+        while (numRoutesToAnalyze > 0) {
             //find new routes by  last selected Route to near nodes
-            newRoutes.addAll( this.findNewRoutesByNearbyNodes(selectedRoute, edges) );
-            
+            newRoutes.addAll(this.findNewRoutesByNearbyNodes(selectedRoute, edges));
+
             //get routes that arrived destiny
             this.addRoutesWithDestiny(destiny, routesFound, newRoutes);
-            
+
             //saved new routes
-            routes.addAll(newRoutes );
+            routes.addAll(newRoutes);
             newRoutes.clear();
-            
+
             //Analyze wich route has less distance
             minDistance = Double.MAX_VALUE;
-            for(RouteDTO routeToAnalyze : routes){
-                if(routeToAnalyze.getDistanceAccumulatedFinal() <= 0 || routeToAnalyze.getDistanceAccumulatedTemp() <= 0){
+            for (RouteDTO routeToAnalyze : routes) {
+                if (routeToAnalyze.getDistanceAccumulatedTemp() <= 0 || routeToAnalyze.getDistanceAccumulatedFinal() > 0) {
                     continue;
                 }
-                
-                if(routeToAnalyze.getDistanceAccumulatedTemp() < minDistance){
+
+                if (routeToAnalyze.getDistanceAccumulatedTemp() < minDistance) {
                     minDistance = routeToAnalyze.getDistanceAccumulatedTemp();
                     selectedRoute = routeToAnalyze;
                 }
             }
-            
+
             //check if others routes have not been verified 
-            numRoutesToAnalyze = (int) routes.stream().filter(xRoute -> xRoute.getDistanceAccumulatedFinal() > 0).count();
-            
+            numRoutesToAnalyze = (int) routes.stream().filter(xRoute -> xRoute.getDistanceAccumulatedFinal() < 0).count();
+
+            if (!routesFound.isEmpty()) {
+                //find unique routes
+                Set<String> routesAsString = routesFound.stream()
+                        .map(RouteDTO::getNodes)
+                        .map(ltsNodes
+                                -> ltsNodes.stream()
+                                .map(NodeDTO::getName)
+                                .reduce("", (partialString, element) -> partialString + element + ",")
+                        )
+                        .collect(Collectors.toSet());
+
+                //check if there are duplicates routes, so break loop
+                Boolean uniqueRoutes = true;
+                for (String routeAsString : routesAsString) {
+                    uniqueRoutes = routesAsString.stream()
+                            .filter(routeS -> routeS.contains(routeAsString.substring(2)) && !routeS.equals(routeAsString))
+                            .findFirst()
+                            .isEmpty();
+
+                    //flag to determinate if some route is inside of other route
+                    if (!uniqueRoutes) {
+                        numRoutesToAnalyze = 0;
+                        break;
+                    }
+                }
+            }
+
         }
-        
-        optimalRoute = Optional.of(selectedRoute);
-        
+
+        minDistance = Double.MAX_VALUE;
+        for (RouteDTO routeFound : routesFound) {
+            if (routeFound.getDistanceAccumulatedFinal() < minDistance && routeFound.getDistanceAccumulatedFinal() > 0) {
+                minDistance = routeFound.getDistanceAccumulatedFinal();
+                optimalRoute = Optional.of(routeFound);
+            }
+        }
+
         return optimalRoute;
     }
-    
+
     private RouteDTO buildInitRoute(NodeDTO origin) {
         return RouteDTO.builder()
                 .distanceAccumulatedTemp(0D)
@@ -119,10 +154,10 @@ public class ProviderDistanceBetweenNodesOptimalRoute extends ProviderRouteBase
         if (nodes == null || nodes.size() != 2) {
             throw new IllegalArgumentException("nodes require just 2 items: origin and destiny");
         }
-        
+
         if (edges == null || edges.isEmpty()) {
             throw new IllegalArgumentException("edges is empty");
         }
     }
-    
+
 }
